66f795b30837bf4f2975ad5e74139a4e4feb1d1a
Samantha <sburkey@wvup.edu>
2019-05-27 16:53:27 -0400

    Added controllers for models

--- PurchaseRequisition/PurchaseRequisition/Data/ApplicationDbContext.cs
+++ PurchaseRequisition/PurchaseRequisition/Data/ApplicationDbContext.cs
@@ -13,5 +13,16 @@ namespace PurchaseRequisition.Data
             : base(options)
         {
         }
+        public DbSet<PurchaseRequisition.Models.Address> Address { get; set; }
+        public DbSet<PurchaseRequisition.Models.Budget> Budget { get; set; }
+        public DbSet<PurchaseRequisition.Models.Campus> Campus { get; set; }
+        public DbSet<PurchaseRequisition.Models.College> College { get; set; }
+        public DbSet<PurchaseRequisition.Models.Department> Department { get; set; }
+        public DbSet<PurchaseRequisition.Models.Division> Division { get; set; }
+        public DbSet<PurchaseRequisition.Models.Item> Item { get; set; }
+        public DbSet<PurchaseRequisition.Models.Order> Order { get; set; }
+        public DbSet<PurchaseRequisition.Models.Role> Role { get; set; }
+        public DbSet<PurchaseRequisition.Models.Status> Status { get; set; }
+        public DbSet<PurchaseRequisition.Models.Vendor> Vendor { get; set; }
     }
 }
