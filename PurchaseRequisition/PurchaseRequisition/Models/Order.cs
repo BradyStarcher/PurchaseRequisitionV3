d4ad4c0e7a3200a937df303c153986b92426d446
Samantha <sburkey@wvup.edu>
2019-05-28 20:56:01 -0400

    Added new migration

--- PurchaseRequisition/PurchaseRequisition/Models/Order.cs
+++ PurchaseRequisition/PurchaseRequisition/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace PurchaseRequisition.Models
     {
         public int ID { get; set; }
 
+        [DefaultValue("In Process")]
         public string Status { get; set; }
 
         public string BusinessJustification { get; set; }
